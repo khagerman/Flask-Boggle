dbe84b8148da5c3094bbf25c50d3749f949db11e
khagerman <71734063+khagerman@users.noreply.github.com>
2021-04-15 13:34:57 -0500

    changing to object oriented program

--- static/app.js
+++ static/app.js
@@ -1,93 +1,86 @@
-let score = 0;
-let count = 60;
-let words = [];
-let topScore = [];
-
-async function checkUserWord(e) {
-  e.preventDefault();
-  timer();
-  const word = $("#submitword input").val().toLowerCase();
-  console.log(word);
-  if (word.length === 0) {
-    return;
+class BoggleGame {
+  constructor(count = 60) {
+    this.score = 0;
+    this.count = count;
+    this.words = [];
+    this.board = $(".board");
+    $("#submitword", this.board).on("submit", this.checkUserWord.bind(this));
+    this.counter = setInterval(this.timer.bind(this), 1000);
   }
-  if (words.includes(word)) {
-    alertUser("alert-info", "Oops, you already found that!");
-    hide();
-    return;
+  async checkUserWord(e) {
+    e.preventDefault();
+    if (this.words.length === 0) {
+      this.timer();
+      this.alertUser("alert-info", "djhsh");
+    }
+    const word = $("#submitword input").val().toLowerCase();
+    console.log(word);
+    if (word.length === 0) {
+      return;
+    }
+    if (this.words.includes(word)) {
+      alertUser("alert-info", "Oops, you already found that!");
+      this.hide();
+      return;
+    }
+
+    const res = await axios.get("/checkword", { params: { word: word } });
+    console.log(res);
+    if (res.data.result === "ok") {
+      this.alertUser("alert-success", "Correct!! Nice Job!");
+      this.words.push(word);
+      this.keepScore(word);
+      this.hide();
+    } else if (res.data.result === "not-on-board") {
+      this.alertUser("alert-warning", "That is a word, but it's not on board!");
+      this.hide();
+    } else {
+      alertUser("alert-danger", "That's not a word  in our dictionary :(");
+      this.hide();
+    }
+    $("#submitword input").val("");
   }
 
-  const res = await axios.get("/checkword", { params: { word: word } });
-  console.log(res);
-  if (res.data.result === "ok") {
-    alertUser("alert-success", "Correct!! Nice Job!");
-    words.push(word);
-    keepScore(word);
-    hide();
-  } else if (res.data.result === "not-on-board") {
-    alertUser("alert-warning", "That is a word, but it's not on board!");
-
-    hide();
-  } else {
-    alertUser("alert-danger", "That's not a word  in our dictionary :(");
-    hide();
+  keepScore(word) {
+    this.score += word.length;
+    $("#points").text(this.score);
   }
 
-  $("#submitword input").val("");
-}
-$("#submitword").on("submit", checkUserWord);
-
-function keepScore(word) {
-  score += word.length;
-  $("#points").text(score);
-}
-
-function hide() {
-  setTimeout(function () {
-    $("#notify").hide();
-    $("#notify").removeClass("alert alert-danger");
-    $("#notify").removeClass("alert alert-warning");
-    $("#notify").removeClass("alert alert-success");
-    $("#notify").removeClass("alert alert-info");
-  }, 3000);
-}
-
-let counter = setInterval(timer, 1000);
-
-function timer() {
-  count = count - 1;
-  if (count <= 0) {
-    $("#timer").html("time's up!");
-    clearInterval(counter);
-    $("input").prop("disabled", true);
-    finishGame();
-    return;
+  hide() {
+    setTimeout(function () {
+      $("#notify").hide();
+      $("#notify").removeClass("alert alert-danger");
+      $("#notify").removeClass("alert alert-warning");
+      $("#notify").removeClass("alert alert-success");
+      $("#notify").removeClass("alert alert-info");
+    }, 3000);
   }
-  $("#timer").html(count);
-}
-function alertUser(type, message) {
-  $("#notify").show().addClass(`alert ${type}`).html(`<h2>${message}<h2>`);
-}
 
-async function finishGame() {
-  const res = await axios.post("/savescore", { score: score });
-  if (res.data.bestscore) {
-    alertUser(
-      "alert-success",
-      `Yah! Congrats new top score of ${score} points !!!!`
-    );
-  } else {
-    alertUser("alert-info", `Nice Job, you scored ${score} points`);
+  timer() {
+    this.count = this.count - 1;
+    if (this.count <= 0) {
+      $("#timer").html("time's up!");
+      clearInterval(this.counter);
+      $("input").prop("disabled", true);
+      this.finishGame();
+      return;
+    }
+    $("#timer").html(this.count);
   }
-  $("#restart").show();
+  alertUser(type, message) {
+    $("#notify").show().addClass(`alert ${type}`).html(`<h2>${message}<h2>`);
+  }
+
+  async finishGame() {
+    const res = await axios.post("/savescore", { score: score });
+    if (res.data.bestscore) {
+      this.alertUser(
+        "alert-success",
+        `Yah! Congrats new top score of ${score} points !!!!`
+      );
+    } else {
+      this.alertUser("alert-info", `Nice Job, you scored ${score} points`);
+    }
+    $("#restart").show();
+  }
 }
-// function checkTopScore() {
-//   let bestScore = Math.max(...topScore);
-//   if (bestScore > score) {
-//     alertUser("alert-success", "Yah! Congrats new top score!!!!");
-//   }
-// }
-function restartGame() {
-  location.reload();
-}
-$("#restart").on("click", restartGame);
